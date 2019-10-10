21777befaa1da279480a243cc2c853c63e88bdc6
Alice <ani00@stuy.edu>
2019-10-09 22:03:35 -0400

    added 17_csvdb

---
+++ 17_csv2db/k17_carepkg/db_builder.py
@@ -0,0 +1,46 @@
+#Alice Ni, Hilary Zen
+#SoftDev  
+#K17 :: SQLITE3 BASICS
+#Oct 2019
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+
+#c.execute("CREATE TABLE courses(code TEXT, mark INTEGER, id INTEGER)")
+f = open("courses.csv","r")
+reader = csv.DictReader(f)
+for row in reader:
+    code = str(row["code"])
+    mark = int(row["mark"])
+    idd = int(row["id"])
+    c.execute("INSERT INTO courses VALUES(?,?,?)",(code,mark,idd))
+f.close()
+    
+
+#c.execute("CREATE TABLE students(name TEXT, age INTEGER, id INTEGER)")
+g = open("students.csv","r")
+geader = csv.DictReader(g)
+for gow in geader:
+    name = str(gow["name"])
+    age = int(gow["age"])
+    iddd = int(gow["id"])
+    c.execute("INSERT INTO students VALUES(?,?,?)",(name,age,iddd))
+g.close()
+
+command = ""          # test SQL stmt in sqlite3 shell, save as string
+c.execute(command)    # run SQL statement
+
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
+
+
