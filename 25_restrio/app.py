a6a44e4569af541001af8ae893b38bd76385d3a2
Alice <ani00@stuy.edu>
2019-11-13 21:54:13 -0500

    added to 25_restrio

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -4,6 +4,11 @@ import urllib3, json
 app = Flask(__name__)
 @app.route('/')
 def hello():
+    return render_template("index.html", text="Try these routes: /pika, /taco, /countries")
+
+
+@app.route('/pika')
+def pokemon():
     http = urllib3.PoolManager()
     response = http.request(
         'GET', "https://pokeapi.co/api/v2/pokemon/pikachu/")
@@ -11,7 +16,7 @@ def hello():
 
     link = json.loads(data)["sprites"]["back_default"]
 
-    return render_template("index.html", image=link)
+    return render_template("index.html", image=link, text="Back of pikachu")
 
 @app.route('/taco')
 def taco():
@@ -24,6 +29,16 @@ def taco():
 
     return render_template("index.html", text=link)
 
+@app.route('/countries')
+def country():
+    http = urllib3.PoolManager()
+    response = http.request(
+        'GET', "https://restcountries.eu/rest/v2/name/united")
+    data = response.data
+
+    link = json.loads(data)[0]["name"]
+
+    return render_template("index.html", text=link)
 
 if __name__ == '__main__':
     app.debug = True
