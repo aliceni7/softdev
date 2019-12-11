d36aba46a852c3135550de35393b45de29b7f9cf
Alice <ani00@stuy.edu>
2019-12-10 20:34:35 -0500

    finished 27_js

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -14,3 +14,20 @@ var fib = function(n){
     }
     return 0;
 }
+
+var gcd = function(a,b){
+    while( b ){
+	var x = b;
+	b = a % x;
+	a = x;
+    }
+    return a;
+}
+
+var studentlist = ["Bob", "Jane", "Ed", "Chan", "Jason", "Hoiley", "TP", "Steve", "Vivi"];
+
+var randomStudent = function(a){
+    var l = a.length;
+    var rand = Math.floor(Math.random(l) * l + 1);
+    return a[rand - 1];
+}
