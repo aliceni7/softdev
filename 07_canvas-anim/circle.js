63d7875cf4b3e94d0b1c7567e9feda3e5c4ca9c3
Alice Ni <ani00@stuy.edu>
2020-02-13 14:40:21 -0500

    fixed

--- 07_canvas-anim/circle.js
+++ 07_canvas-anim/circle.js
@@ -1,29 +1,45 @@
+//Leia Park & Alice Ni
+//SoftDev1 pd9
+//K07 -- They lock us in the tower whenever we get caught ...which is often
+//2020-02-12
+
 var canvas = document.getElementById("anim");
 var ctx = canvas.getContext("2d");
 
+var radius = 5;
+var change = 1;
+var id = 0;
 
-r = 0;
 
-var drawCircle = function(e) {
-    while (r <= 250){
-	ctx.beginPath();
-	ctx.arc(250, 250, r, 0, 2 * Math.PI);
-	ctx.stroke();
-	ctx.fillStyle = "#ff0000";
-	ctx.fill();
-	r++;
-    }
-    while (r >= 0){
-	ctx.beginPath();
-	ctx.arc(250, 250, r, 0, 2 * Math.PI);
-	ctx.stroke();
-	ctx.fillStyle = "#ff0000";
-	ctx.fill();
-	r--;
-    }
+function clear() {
+    ctx.clearRect(0, 0, 500, 500);
 }
 
-window.requestAnimationFrame(drawCircle);
+function startAnim() {
+    window.cancelAnimationFrame(id); //necessary because it stops the previous requestAnimationFrame call, so it prevents the animation speed from increasing
+    if (radius == 250) {
+	change = -1;
+    }
+    if (radius == 0) {
+	change = 1;
+    }
+    clear();
+    ctx.beginPath();
+    ctx.arc(250, 250, radius += change, 0, 2 * Math.PI);
+    ctx.fillStyle = "#f5df04";
+    ctx.fill();
+    id = window.requestAnimationFrame(startAnim);
+};
 
-var start = document.getElementById("start");
-start.addEventListener('click', drawCircle);
+function stopAnim() {
+    window.cancelAnimationFrame(id);
+}
+
+
+var buttonStart = document.getElementById("start");
+buttonStart.addEventListener('click', function(e) {
+    id = window.requestAnimationFrame(startAnim);
+});
+
+var buttonStop = document.getElementById("stop");
+buttonStop.addEventListener('click', stopAnim);
