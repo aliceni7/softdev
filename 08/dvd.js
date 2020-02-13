2ebb12efa88554a28631784e47f4657b6dc21b02
Alice Ni <ani00@stuy.edu>
2020-02-13 15:35:09 -0500

    k

--- 08/dvd.js
+++ 08/dvd.js
@@ -12,11 +12,13 @@ var clearCanvas = function(e) {
 
 
 var id = 0;
-var x = 0;
-var y = 0;
+var x = 15;
+var y = 15;
+var moveX = 2;
+var moveY = 7;
 var moveDVD = true;
-//var logo = new Image();
-//logo.src = "logo_dvd.jpg";
+var logo = new Image();
+logo.src = "logo_dvd.jpg";
 
 var startAnim = function(e) {
     window.cancelAnimationFrame(id);
@@ -24,13 +26,25 @@ var startAnim = function(e) {
 	console.log(moveDVD);
 	clearCanvas();
 	ctx.beginPath();
-	ctx.arc(x += 1, y += 1, 5, 0, 2 * Math.PI);
-	ctx.fillStyle = "#f5df04";
-	ctx.fill();
-	//ctx.drawImage(logo.src, x += 1, y += 1);
+	ctx.drawImage(logo, x += moveX, y += moveY);
+	console.log(x);
+	console.log(y);
+	if (x >= 470 || x <= 0) { // also not completely random path yet
+	    moveX *= -1;
+	}
+	if (y >= 470 || y <= 0) { // does not bounce off the right wall properly
+	    moveY *= -1;
+	}
     }
     id = window.requestAnimationFrame(startAnim);
 }
 
-var buttonStart = document.getElementById("start");
+var stopAnim = function(e) {
+    window.cancelAnimationFrame(id);
+}
+
+var buttonStart = document.getElementById("movie");
 buttonStart.addEventListener('click', startAnim);
+
+var buttonStop = document.getElementById("stop");
+buttonStop.addEventListener('click', stopAnim);
