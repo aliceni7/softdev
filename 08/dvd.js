7d940b55630297860fe09f7514d1fb82fbca1791
Alice Ni <ani00@stuy.edu>
2020-02-14 15:00:16 -0500

    better

--- 08/dvd.js
+++ 08/dvd.js
@@ -11,40 +11,70 @@ var clearCanvas = function(e) {
 }
 
 
-var id = 0;
-var x = 15;
-var y = 15;
-var moveX = 2;
-var moveY = 7;
+var dvdid = 0;
+var circleid = 0;
+var radius = 5;
+var change = 1;
+var x = Math.floor(Math.random() * 470);
+var y = Math.floor(Math.random() * 470);
+var moveX = 1;
+var moveY = 1;
 var moveDVD = true;
 var logo = new Image();
 logo.src = "logo_dvd.jpg";
 
-var startAnim = function(e) {
-    window.cancelAnimationFrame(id);
+
+var startCircleAnim = function(e) {
+    window.cancelAnimationFrame(circleid); //necessary because it stops the previous requestAnimationFrame call, so it prevents the animation speed from increasing
+    window.cancelAnimationFrame(dvdid);
+    if (radius == 250) {
+	change = -1;
+    }
+    if (radius == 0) {
+	change = 1;
+    }
+    clearCanvas();
+    ctx.beginPath();
+    ctx.arc(250, 250, radius += change, 0, 2 * Math.PI);
+    ctx.fillStyle = "#f5df04";
+    ctx.fill();
+    circleid = window.requestAnimationFrame(startCircleAnim);
+};
+
+
+var startMovieAnim = function(e) {
+    window.cancelAnimationFrame(dvdid);
+    window.cancelAnimationFrame(circleid);
     if (moveDVD) {
-	console.log(moveDVD);
+	//x = Math.floor(Math.random() * 470);
+	//y = Math.floor(Math.random() * 470);
 	clearCanvas();
 	ctx.beginPath();
 	ctx.drawImage(logo, x += moveX, y += moveY);
 	console.log(x);
 	console.log(y);
-	if (x >= 470 || x <= 0) { // also not completely random path yet
+	if (x >= ( canvas.width - 60) || x <= 0) { // also not completely random path yet
 	    moveX *= -1;
 	}
-	if (y >= 470 || y <= 0) { // does not bounce off the right wall properly
+	if (y >= ( canvas.width - 49) || y <= 0) { // does not bounce off the right wall properly
 	    moveY *= -1;
 	}
     }
-    id = window.requestAnimationFrame(startAnim);
+    dvdid = window.requestAnimationFrame(startMovieAnim);
 }
 
 var stopAnim = function(e) {
-    window.cancelAnimationFrame(id);
+    window.cancelAnimationFrame(dvdid);
+    window.cancelAnimationFrame(circleid);
 }
 
 var buttonStart = document.getElementById("movie");
-buttonStart.addEventListener('click', startAnim);
+buttonStart.addEventListener('click', startMovieAnim);
 
 var buttonStop = document.getElementById("stop");
 buttonStop.addEventListener('click', stopAnim);
+
+var b = document.getElementById("circle");
+b.addEventListener('click', startCircleAnim);
+
+
