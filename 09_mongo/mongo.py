322543c63418c0b4fa9f7d504d4b8899afe28804
Alice Ni <ani00@stuy.edu>
2020-02-27 15:28:09 -0500

    code

--- 09_mongo/mongo.py
+++ 09_mongo/mongo.py
@@ -6,7 +6,7 @@ db = client['testdata'] # does not have to exist
 col = db['restaurants']
 
 if (col.count() == 0):
-    file = open("primer-dataset.json", "r")
+    file = open("dataset.json", "r")
     content = file.readlines()
     for line in content:
         col.insert_one(loads(line))
@@ -15,5 +15,29 @@ def borough(b):
     for r in col.find({ "borough": b }):
         pprint.pprint(r)
 
-borough("Queens")
+def zipcode(z):
+    for r in col.find({ "address.zipcode": z }):
+        pprint.pprint(r)
 
+def grade(z, g):
+    for r in col.find({ "address.zipcode": z }):
+       # print(r)
+        record = r['grades']
+       # print(record)
+    #    print(record[len(record)-1])
+        if (record[len(record)-1]['grade'] == g):
+            pprint.pprint(r)
+
+def thres(z, g):
+    for r in col.find({ "address.zipcode": z }):
+        record = r['grades']
+        for i in range(len(record)):
+            if (record[i]['grade'] > int(g)):
+                pprint.pprint(r)
+
+### TEST CASES
+borough("Manhattan")
+#zipcode("10014")
+#grade("10014", 'A')
+
+client.close() # at the very end !!!
